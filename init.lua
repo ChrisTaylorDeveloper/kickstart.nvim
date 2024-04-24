7f7c63b599e7e72efa316f4a1352b1fa88fa3995
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-04-24 10:21:44 +0200

    Remove github theme plugin.

--- init.lua
+++ init.lua
@@ -307,19 +307,6 @@ require('lazy').setup({
 
   { 'pocco81/auto-save.nvim', opts = {} },
 
-  {
-    'projekt0n/github-nvim-theme',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require('github-theme').setup {
-        -- ...
-      }
-
-      vim.cmd 'colorscheme github_light'
-    end,
-  },
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
