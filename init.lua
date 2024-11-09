8553f72fceba8cd31e65331afd8c5cc470df73f7
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-11-09 08:56:31 +0100

    Disable leap plugin for now.

--- init.lua
+++ init.lua
@@ -320,14 +320,14 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'ggandor/leap.nvim',
-    config = function()
-      vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-      vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
-      vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
-    end,
-  },
+  -- {
+  --   'ggandor/leap.nvim',
+  --   config = function()
+  --     vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+  --     vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+  --     vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
+  --   end,
+  -- },
 
   { 'hrsh7th/cmp-buffer' },
 
