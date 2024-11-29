d0bb1b508829749718295552b9cc1f2dad190297
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-11-29 16:27:35 +0000

    Attempt to add my own snippets.

--- init.lua
+++ init.lua
@@ -857,6 +857,7 @@ require('lazy').setup({
             'rafamadriz/friendly-snippets',
             config = function()
               require('luasnip.loaders.from_vscode').lazy_load()
+              require('luasnip.loaders.from_vscode').lazy_load { paths = { '/home/chris/cgt/notes/snippets' } }
             end,
           },
         },
