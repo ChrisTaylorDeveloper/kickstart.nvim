3770570482fe4b5e1965f84a3403bf29c1fa1deb
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-02-14 13:07:44 +0300

    Tabline and Laststatus conf.

--- init.lua
+++ init.lua
@@ -98,6 +98,9 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+vim.o.showtabline = 2
+vim.opt.laststatus = 3
+
 vim.opt.wrap = false
 vim.opt.linebreak = false
 vim.opt.swapfile = false
