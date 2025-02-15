0d7e41877bccb24bdd63d5f8b7a7d3e9a10baf71
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-02-15 09:37:03 +0300

    Add a comment back in.

--- init.lua
+++ init.lua
@@ -1033,6 +1033,7 @@ require('lazy').setup({
 
   { 'pocco81/auto-save.nvim', opts = {} },
 
+  -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
   { -- Collection of various small independent plugins/modules
