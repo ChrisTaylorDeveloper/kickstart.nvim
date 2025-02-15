c34cc69eb065c111cd09ea5091c908ca4e62774f
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-02-15 09:32:55 +0300

    Collapse line, to make diff easier to read.

--- init.lua
+++ init.lua
@@ -814,16 +814,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = {
-          axaml = true,
-          xml = true,
-          cs = true,
-          js = true,
-          dockerfile = true,
-          php = true,
-          c = true,
-          cpp = true,
-        }
+        local disable_filetypes = { axaml = true, xml = true, cs = true, js = true, dockerfile = true, php = true, c = true, cpp = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
