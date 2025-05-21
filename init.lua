5d654032679732ab7b3ed5216cd754d54fa05b7a
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-05-21 18:18:46 +0100

    Remove too fussy mappings.

--- init.lua
+++ init.lua
@@ -234,14 +234,8 @@ vim.keymap.set({ 'n', 'i', 'v' }, '<A-8>', '<cmd> resize +1 <cr>')
 vim.keymap.set({ 'n', 'i', 'v' }, '<A-9>', '<cmd> vertical resize -1 <cr>')
 vim.keymap.set({ 'n', 'i', 'v' }, '<A-0>', '<cmd> vertical resize +1 <cr>')
 
-vim.keymap.set('i', '<A-j>', '<Esc>:m .+1<CR>==gi', { desc = 'Move line down, in Insert mode.' })
-vim.keymap.set('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { desc = 'Move line up, in Insert mode.' })
-
 vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move line down, in Normal mode.' })
 vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up, in Normal mode.' })
-
-vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv-gv", { desc = 'Move line down, in Visual mode.' })
-vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv-gv", { desc = 'Move line up, in Visual mode.' })
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
